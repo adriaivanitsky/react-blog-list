3b6ac7ff1d3dfd718676e151b7494c1c81397024
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-06 15:19:26 -0800

    got page to render

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       //important to await this data because when we get the data is it coming from an async function
       const data = await getBlogs();
       setBlogs(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
+  if (loading) return <h1>Loading...</h1>;
   return (
     <section className="main">
       <Header />
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
       <Footer />
     </section>
   );
